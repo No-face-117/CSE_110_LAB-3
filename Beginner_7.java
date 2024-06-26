090854bc0aa100b84a62bd3496b7a6c75a7b18f5
No-face-117 <sayedmahamudalif@gmail.com>
2024-06-26 23:13:55 +0600

    Add files via upload

---
+++ Beginner_7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Beginner_7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter an integer : ");
+        int x = scan.nextInt();
+        
+        if (x < 0) {
+            System.out.println("output: " + (2*x));
+        }
+        else if (0<=x && x<2) {
+            System.out.println("output: " + (2+x));
+        }
+        else if (2<=x && x<5) {
+            double mass = Math.pow(x, 2) - 1;
+            System.out.println("output: " + mass);
+        }
+        else if (x >= 5) {
+            double masse = 3*Math.pow(x, 2) + 2;
+            System.out.println("output: " + masse);
+        }
+        else{}
+
+
+        
+
+        scan.close();
+    }
+}
