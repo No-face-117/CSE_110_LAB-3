add860d9283c3c7f9771580d54c53eed1e3a40d9
No-face-117 <sayedmahamudalif@gmail.com>
2024-06-28 23:19:39 +0600

    Beginner_3.java

--- Beginner_3.java
+++ Beginner_3.java
@@ -2,63 +2,42 @@ import java.util.Scanner;
 
 public class Beginner_3 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the first number : ");
-        int num_1 = scan.nextInt();
+        System.out.println("Enter the first number: ");
+        int num1 = scanner.nextInt();
 
-        System.out.println("Enter the second number : ");
-        int num_2 = scan.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = scanner.nextInt();
 
-        scan.nextLine();
-
-        System.out.println("What do you want to do?");
-        System.out.println("add : '+'");
-        System.out.println("subtract : '-'");
-        System.out.println("multiply : '*'");
-        System.out.println("divide : '/'");
-        System.out.println();
-        System.out.println("so -=> add / subtract / multiply / divide");
-        String score = scan.nextLine();
-
-        System.out.println();
-        System.out.println("Number 1 is : " + num_1);
-        System.out.println("Number 2 is : " + num_2);
-        System.out.println();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        String operator = scanner.next();
 
         int result = 0;
         boolean validOperation = true;
 
-        switch (score) {
-            case "add":
-                result = num_1 + num_2;
-                break;
-            case "subtract":
-                result = num_1 - num_2;
-                break;
-            case "multiply":
-                result = num_1 * num_2;
-                break;
-            case "divide":
-                if (num_2 != 0) {
-                    result = num_1 / num_2;
-                } else {
-                    System.out.println("Division by zero is not allowed!");
-                    validOperation = false;
-                }
-                break;
-            default:
-                System.out.println("Invalid operation!");
+        if (operator.equals("+")) {
+            result = num1 + num2;
+        } else if (operator.equals("-")) {
+            result = num1 - num2;
+        } else if (operator.equals("*")) {
+            result = num1 * num2;
+        } else if (operator.equals("/")) {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
                 validOperation = false;
-                break;
+            }
+        } else {
+            System.out.println("Error: Invalid operator. Please use +, -, *, or /.");
+            validOperation = false;
         }
 
         if (validOperation) {
-            System.out.println("The result of the operation is: " + result);
+            System.out.println("The result is: " + result);
         }
 
-        scan.close();
+        scanner.close();
     }
 }
-
-
