4c44ce15c3983161a99f25424b5b2b0fde1133f8
No-face-117 <sayedmahamudalif@gmail.com>
2024-07-01 16:02:29 +0600

    Expert_12.java

--- Expert_12.java
+++ Expert_12.java
@@ -13,7 +13,48 @@ public class Expert_12 {
         if (amount_given > amount_to_be_paid) {
             int change = amount_given - amount_to_be_paid;
             System.out.println("Change to be returned: " + change + " taka");
-            calculate_change(change);
+
+            if (change >= 100) {
+                int count = change / 100;
+                change %= 100;
+                System.out.println("100 taka note: " + count);
+            }
+
+            if (change >= 50) {
+                int count = change / 50;
+                change %= 50;
+                System.out.println("50 taka note: " + count);
+            }
+
+            if (change >= 20) {
+                int count = change / 20;
+                change %= 20;
+                System.out.println("20 taka note: " + count);
+            }
+
+            if (change >= 10) {
+                int count = change / 10;
+                change %= 10;
+                System.out.println("10 taka note: " + count);
+            }
+
+            if (change >= 5) {
+                int count = change / 5;
+                change %= 5;
+                System.out.println("5 taka coin: " + count);
+            }
+
+            if (change >= 2) {
+                int count = change / 2;
+                change %= 2;
+                System.out.println("2 taka coin: " + count);
+            }
+
+            if (change >= 1) {
+                int count = change / 1;
+                change %= 1;
+                System.out.println("1 taka coin: " + count);
+            }
         } else if (amount_given < amount_to_be_paid) {
             int amount_needed = amount_to_be_paid - amount_given;
             System.out.println("The customer needs to pay: " + amount_needed + " taka");
@@ -22,26 +63,5 @@ public class Expert_12 {
         }
 
         scanner.close();
-    }
-
-    public static void calculate_change(int change) {
-        int[] notes = {100, 50, 20, 10};
-        int[] coins = {5, 2, 1};
-        
-        for (int note : notes) {
-            if (change >= note) {
-                int count = change / note;
-                change %= note;
-                System.out.println(note + " taka note: " + count);
-            }
-        }
-
-        for (int coin : coins) {
-            if (change >= coin) {
-                int count = change / coin;
-                change %= coin;
-                System.out.println(coin + " taka coin: " + count);
-            }
-        }
     }
 }
