f336020b6c0ed162a39d3f6dee5bfed4017a7316
No-face-117 <sayedmahamudalif@gmail.com>
2024-07-01 20:06:55 +0600

    Beginner_4.java

--- Beginner_4.java
+++ Beginner_4.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+mport java.util.Scanner;
 
 public class Beginner_4 {
     public static void main(String[] args) {
@@ -9,7 +9,14 @@ public class Beginner_4 {
 
         if (number % 5 == 0 && number % 7 == 0) {
             System.out.println("The number " + number + " is divisible by both 5 and 7.");
-        } else {
+        }
+        else if (number % 5 == 0) {
+            System.out.println("Invalid: Divisible by 5 Only");
+        }
+        else if (number % 7 == 0) {
+            System.out.println("Invalid: Divisible by 7 Only");
+        }
+         else {
             System.out.println("No");
         }
 
